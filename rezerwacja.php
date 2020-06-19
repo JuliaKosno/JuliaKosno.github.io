602e7602113f5ee9febb3530f31bbc1793a03f43
JuliaKosno <64750229+JuliaKosno@users.noreply.github.com>
2020-06-19 14:56:21 +0200

    Update rezerwacja.php

--- rezerwacja.php
+++ rezerwacja.php
@@ -11,31 +11,32 @@
     <body >
  
 <?php
-if (! empty ( $_REQUEST [" email "]) && ! empty ( $_REQUEST ["imie"]) && ! empty ( $_REQUEST [" nazwisko "]) && ! empty ( $_REQUEST ["wydarzenie"])&& ! empty ( $_REQUEST ["data"])) {
-$email = $_REQUEST ["email"] ;
-$imie = $_REQUEST ["imie"] ;
-$nazwisko = $_REQUEST ["nazwisko"] ;
-$wydarzenie = $_REQUEST ["wydarzenie"] ;
-$data = $_REQUEST ["data"] ;
+if (!empty ( $_REQUEST["email"]) && !empty ( $_REQUEST["imie"]) && !empty ( $_REQUEST["nazwisko"]) && !empty ( $_REQUEST["wydarzenie"]) && !empty ( $_REQUEST["data"])) {
+$email = $_REQUEST["email"] ;
+$imie = $_REQUEST["imie"] ;
+$nazwisko = $_REQUEST["nazwisko"] ;
+$wydarzenie = $_REQUEST["wydarzenie"] ;
+$data = $_REQUEST["data"] ;
 } else die (" Niepoprawnie wypełniony formularz!") ;
-
-$string = file_get_contents ("rezerwacje.json ") ;
+/>
+<?php
+$string = file_get_contents("rezerwacje.json") ;
  if ( $string )
  $arr = json_decode ( $string , true ) or die (" Niewłaściwy plik JSON !") ;
  else
  $arr = array () ;
- array_push ( $arr , array (" imie " => $imie ," nazwisko " => $nazwisko , " email " => $email, " wydarzenie " => $wydarzenie, " data " => $data ));
- $string = json_encode ( $arr ) ;
- if( file_put_contents ("rezerwacje.json ", $string ))
+ array_push ( $arr , array ("imie" => $imie ,"nazwisko" => $nazwisko , "email" => $email, "wydarzenie" => $wydarzenie, "data" => $data ));
+ $string = json_encode($arr) ;
+ if( file_put_contents("rezerwacje.json ", $string ))
  echo "Dodane!" ;
  else
  die ("Błąd!") ;
 ?>
 
 <?php
-	$string = file_get_contents(" rezerwacje.json ") ; 
+	$string = file_get_contents("rezerwacje.json") ; 
 	if ( $string ) {
-		$arr = json_decode ( $string , true ) or die (" Niewłaściwy plik JSON !") ;
+		$arr = json_decode($string,true) or die (" Niewłaściwy plik JSON !") ;
 		echo "<table >\n" ;
 		echo "<tr ><th > Imię </th > <th > Nazwisko </th > <th >Email </th ></tr >\n" ;
 		foreach ( $arr as $row ) {
